96701020f3ba5e227f5d70cff2f692d8ff40a5f3
viktord2 <viktord@wix.com>
2023-04-26 09:33:17 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/model/availability/availability-api.ts
@@ -0,0 +1,54 @@
+import { WixSession } from '../auth/auth';
+import { availabilityCalendar } from '@wix/bookings';
+
+export const getServiceAvailability = (
+  wixSession: WixSession,
+  {
+    serviceId,
+    from,
+    to,
+    timezone,
+    slotsPerDay,
+    limit,
+  }: {
+    serviceId: string;
+    from: string;
+    to: string;
+    timezone?: string;
+    slotsPerDay?: number;
+    limit?: number;
+  }
+): Promise<availabilityCalendar.QueryAvailabilityResponse> =>
+  queryAvailability({
+    input: {
+      slotsPerDay,
+      timezone,
+      query: {
+        sort: [
+          {
+            fieldName: 'startTime',
+            order: availabilityCalendar.SortOrder.ASC,
+          },
+        ],
+        filter: {
+          serviceId: [serviceId],
+          startDate: from,
+          endDate: to,
+        },
+        cursorPaging: { ...(limit ? { limit } : {}) },
+      },
+    },
+    wixSession,
+  });
+
+const queryAvailability = ({
+  input,
+  wixSession,
+}: {
+  input: availabilityCalendar.QueryAvailabilityRequest;
+  wixSession: WixSession;
+}) =>
+  wixSession!.wixClient!.availabilityCalendar.queryAvailability(input.query!, {
+    timezone: input.timezone,
+    slotsPerDay: input.slotsPerDay,
+  });
